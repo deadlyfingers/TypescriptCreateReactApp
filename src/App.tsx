089833694f53de9c86ed5e345f8f8e36fd236b46
David Douglas <david@deadlyfingers.com>
2019-11-15 17:13:02 +0000

    :octocat: Create React App Typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <a href="https://github.com/deadlyfingers/TypescriptCreateReactApp">Typescript Create React App</a>
+    </div>
+  )
+}
+
+export default App
